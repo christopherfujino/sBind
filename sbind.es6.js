6e1f9ec2b9e6b08895ff03aeca3529ef4c6e6c7a
Christopher Fujino <christopherfujino@gmail.com>
2016-05-28 22:29:38 -1000

    add sbind.es6.js, the raw es6 code, and sbind.js, the transpiled es5 code

---
+++ sbind.es6.js
@@ -0,0 +1,70 @@
+'use strict';
+
+let sBind = function() { // simple databinding namespace
+  let domStore = {};     // private storage of jQuery references
+  return {
+    update : function(refObj, key) { // the DOM is never queried :D
+      const name = refObj.get('name'), value = refObj.get(key),
+            arr = domStore[name][key];
+      if (!name || value === null || !Array.isArray(arr)) {
+        console.log('Error in paramters to sBind.update()');
+        return false;
+      }
+      arr.forEach($span => { $span.text(value); });
+    },
+    bind : function(args) { // with keys: type, reference, key, $object,
+                            // typeOfEvent, callback, eventData
+      // argument checking
+      if (typeof args !== 'object') {
+        console.log('bad args in sBind.bind()!');
+        return false;
+      }
+      if (typeof args.reference !== 'object') {
+        console.log('bad args.reference to bind from in sBind.bind()');
+        return false;
+      }
+      // meat of code
+      if (args.type === 'input') {
+        if (!(args.$object instanceof jQuery)) {
+          console.log('bad args.$object passed to sBind.bind()!');
+          return false;
+        }
+        if (args.typeOfEvent === 'click') {
+          if (typeof args.eventData === 'object') {
+            args.$object.on(args.typeOfEvent, args.eventData, args.callback);
+          } else {
+            args.$object.on(args.typeOfEvent, args.callback);
+          }
+        }
+        // implement other types of handlers?
+        else {
+          console.log('bad args.typeOfEvent passed to sBind.bind()!');
+          return false;
+        }
+      } else { // default to output
+        if (typeof args.key !== 'string' && typeof args.key !== 'number') {
+          console.log('bad args.key passed to sBind.bind()!');
+          return false;
+        }
+        if (!(args.$object instanceof jQuery)) {
+          args.$object = $('<span>'); // default bind to span
+        }
+        args.$object.text(args.reference.get(args.key));
+      }
+
+      let objectName = args.reference.get('name'),
+          entry = domStore[objectName]; // retrieve the name of the object
+      // here manage our domStore, for use by sBind.update()
+      if (entry) {             // if domStore entry for this object exists
+        if (entry[args.key]) { // if array exists for this particular key
+          entry[args.key].push(args.$object);
+        } else { // object exists in store, but not array for key
+          entry[args.key] = [ args.$object ];
+        }
+      } else { // no domStore entry exists for this object
+        domStore[args.reference.get('name')] = {key : [ args.$object ]};
+      }
+      return args.$object;
+    }
+  };
+}();
---
+++ sbind.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+var sBind = function () {
+  // simple databinding namespace
+  var domStore = {}; // private storage of jQuery references
+  return {
+    update: function update(refObj, key) {
+      // the DOM is never queried :D
+      var name = refObj.get('name'),
+          value = refObj.get(key),
+          arr = domStore[name][key];
+      if (!name || value === null || !Array.isArray(arr)) {
+        console.log('Error in paramters to sBind.update()');
+        return false;
+      }
+      arr.forEach(function ($span) {
+        $span.text(value);
+      });
+    },
+    bind: function bind(args) {
+      // with keys: type, reference, key, $object,
+      // typeOfEvent, callback, eventData
+      // argument checking
+      if ((typeof args === 'undefined' ? 'undefined' : _typeof(args)) !== 'object') {
+        console.log('bad args in sBind.bind()!');
+        return false;
+      }
+      if (_typeof(args.reference) !== 'object') {
+        console.log('bad args.reference to bind from in sBind.bind()');
+        return false;
+      }
+      // meat of code
+      if (args.type === 'input') {
+        if (!(args.$object instanceof jQuery)) {
+          console.log('bad args.$object passed to sBind.bind()!');
+          return false;
+        }
+        if (args.typeOfEvent === 'click') {
+          if (_typeof(args.eventData) === 'object') {
+            args.$object.on(args.typeOfEvent, args.eventData, args.callback);
+          } else {
+            args.$object.on(args.typeOfEvent, args.callback);
+          }
+        }
+        // implement other types of handlers?
+        else {
+            console.log('bad args.typeOfEvent passed to sBind.bind()!');
+            return false;
+          }
+      } else {
+        // default to output
+        if (typeof args.key !== 'string' && typeof args.key !== 'number') {
+          console.log('bad args.key passed to sBind.bind()!');
+          return false;
+        }
+        if (!(args.$object instanceof jQuery)) {
+          args.$object = $('<span>'); // default bind to span
+        }
+        args.$object.text(args.reference.get(args.key));
+      }
+
+      var objectName = args.reference.get('name'),
+          entry = domStore[objectName]; // retrieve the name of the object
+      // here manage our domStore, for use by sBind.update()
+      if (entry) {
+        // if domStore entry for this object exists
+        if (entry[args.key]) {
+          // if array exists for this particular key
+          entry[args.key].push(args.$object);
+        } else {
+          // object exists in store, but not array for key
+          entry[args.key] = [args.$object];
+        }
+      } else {
+        // no domStore entry exists for this object
+        domStore[args.reference.get('name')] = { key: [args.$object] };
+      }
+      return args.$object;
+    }
+  };
+}();
