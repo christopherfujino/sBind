b99b8e62e032f817ca72e4957fd75f6c5e7b2763
Christopher Fujino <christopherfujino@gmail.com>
2016-06-01 20:30:54 -1000

    build of previous commit

--- es5/sbind.js
+++ es5/sbind.js
@@ -24,6 +24,51 @@ var sBind = function () {
       });
       return true;
     },
+    bindOutput: function bindOutput(reference, key, $object) {
+      // reference, key, $object (optional)
+      if ((typeof reference === 'undefined' ? 'undefined' : _typeof(reference)) !== 'object') {
+        console.log('reference parameter passed to sBind.bindOutput() not an object');
+        return false;
+      }
+      if (typeof key !== 'string') {
+        console.log('"key" parameter passed to sBind.bindOutput() not a string');
+        return false;
+      }
+      if (!($object instanceof jQuery)) {
+        $object = $('<span>'); // default bind to span
+      }
+      $object.text(reference.get(key));
+
+      var objectName = reference.get('name'),
+          entry = domStore[objectName]; // retrieve the name of the object
+      // here manage our domStore, for use by sBind.update()
+      if (entry) {
+        // if domStore entry for this object exists
+        if (!entry[key]) {
+          // if array exists for this particular key
+          entry[key] = [];
+        }
+        entry[key].push($object);
+      } else {
+        // no domStore entry exists for this object
+        var rName = args.reference.get('name');
+        domStore[rName] = {};
+        domStore[rName][args.key] = [];
+        domStore[rName][args.key].push(args.$object);
+      }
+    },
+    bindInput: function bindInput($object, typeOfEvent, callback, eventData) {
+      if (!($object instanceof jQuery)) {
+        console.log('bad type of $object passed to sBind.bindInput()');
+        return false;
+      }
+      if ((typeof eventData === 'undefined' ? 'undefined' : _typeof(eventData)) === 'object') {
+        $object.on(typeOfEvent, eventData, callback);
+      } else {
+        $object.on(typeOfEvent, callback);
+      }
+      return $object;
+    },
     bind: function bind(args) {
       // with keys: type, reference, key, $object,
       // typeOfEvent, callback, eventData
