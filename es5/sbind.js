5545c86ef74387c4ff617e0b49e157671c7da50d
Christopher Fujino <christopherfujino@gmail.com>
2016-06-02 17:09:14 -1000

    code cleanup

--- es5/sbind.js
+++ es5/sbind.js
@@ -25,7 +25,6 @@ var sBind = function () {
       return true;
     },
     bindOutput: function bindOutput(reference, key, $object) {
-      // reference, key, $object (optional)
       if ((typeof reference === 'undefined' ? 'undefined' : _typeof(reference)) !== 'object') {
         console.log('reference parameter passed to sBind.bindOutput() not an object');
         return false;
@@ -69,71 +68,6 @@ var sBind = function () {
         $object.on(typeOfEvent, callback);
       }
       return $object;
-    },
-    bind: function bind(args) {
-      // with keys: type, reference, key, $object,
-      // typeOfEvent, callback, eventData
-      // argument checking
-      if ((typeof args === 'undefined' ? 'undefined' : _typeof(args)) !== 'object') {
-        console.log('bad args in sBind.bind()!');
-        return false;
-      }
-      // meat of code
-      if (args.type === 'input') {
-        // if input
-        if (!(args.$object instanceof jQuery)) {
-          console.log('bad args.$object passed to sBind.bind()!');
-          return false;
-        }
-        if (args.typeOfEvent === 'click') {
-          if (_typeof(args.eventData) === 'object') {
-            args.$object.on(args.typeOfEvent, args.eventData, args.callback);
-          } else {
-            args.$object.on(args.typeOfEvent, args.callback);
-          }
-        }
-        // implement other types of handlers?
-        else {
-            console.log('bad args.typeOfEvent passed to sBind.bind()!');
-            return false;
-          }
-      } else {
-        // default to output
-        if (_typeof(args.reference) !== 'object') {
-          // only output needs this reference
-          console.log('bad args.reference to bind from in sBind.bind()');
-          return false;
-        }
-        if (typeof args.key !== 'string' && typeof args.key !== 'number') {
-          console.log('bad args.key passed to sBind.bind()!');
-          return false;
-        }
-        if (!(args.$object instanceof jQuery)) {
-          args.$object = $('<span>'); // default bind to span
-        }
-        args.$object.text(args.reference.get(args.key));
-
-        var objectName = args.reference.get('name'),
-            entry = domStore[objectName]; // retrieve the name of the object
-        // here manage our domStore, for use by sBind.update()
-        if (entry) {
-          // if domStore entry for this object exists
-          if (entry[args.key]) {
-            // if array exists for this particular key
-            entry[args.key].push(args.$object);
-          } else {
-            // object exists in store, but not array for key
-            entry[args.key] = [args.$object];
-          }
-        } else {
-          // no domStore entry exists for this object
-          var rName = args.reference.get('name');
-          domStore[rName] = {};
-          domStore[rName][args.key] = [];
-          domStore[rName][args.key].push(args.$object);
-        }
-      }
-      return args.$object;
     }
   };
 }();
